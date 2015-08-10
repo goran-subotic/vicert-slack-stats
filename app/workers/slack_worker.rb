c6444f9947ea3d760bfdf5470950a268d790ac05
slobar <slobodan.radosavljevic@vicert.com>
2015-08-10 17:29:40 +0200

    Code re-factoring

--- app/workers/slack_worker.rb
+++ app/workers/slack_worker.rb
@@ -7,36 +7,10 @@ class SlackWorker
 
   def perform(name)
     
-    uriUsers = URI(Rails.configuration.x.slack_users_uri)
-    uriMessages = URI(Rails.configuration.x.slack_search_all_uri)
-    params = { :token => Rails.configuration.x.slack_key}
-    uriUsers.query = URI.encode_www_form(params)
-
-    Net::HTTP.start(uriUsers.host, uriUsers.port, :use_ssl => uriUsers.scheme == 'https', :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |httpUsers|
-      request = Net::HTTP::Get.new uriUsers
-
-      response = httpUsers.request request
-      data = JSON.parse(response.body)
-      
-      
-      data['members'].each do |member|
-        if !(Stat.where(:name => member['name']).exists?)
-          Stat.create(:name=> member['name'])
-        end
-        params = { :token => Rails.configuration.x.slack_key, :query => 'from:'+member['name']}
-        uriMessages.query = URI.encode_www_form(params)
-        Net::HTTP.start(uriMessages.host, uriMessages.port, :use_ssl => uriMessages.scheme == 'https', :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |httpMsg|
-          request = Net::HTTP::Get.new uriMessages
-
-          response = httpMsg.request request
+    users = StatHelper::fetch_users_from_slack
+    StatHelper::insert_users(users)
+    StatHelper::update_users_messages
     
-          dataUser = JSON.parse(response.body)
-          Stat.where(:name => member['name']).update_all(:msg_count => dataUser['messages']["total"])
-        end
-      end
-        
-    end
   end
-  
   
 end
