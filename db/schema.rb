32f9602fef3301d8fe6c0aabe8b85fc62b5636a5
Goran Subotic <goran.subotic@vicert.com>
2015-08-11 09:53:20 +0200

    small changes.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150810132852) do
+ActiveRecord::Schema.define(version: 20150810130734) do
 
   create_table "delayed_jobs", force: :cascade do |t|
     t.integer  "priority",   default: 0, null: false
