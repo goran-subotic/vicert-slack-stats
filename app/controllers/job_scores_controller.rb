64b8159b445b30e1969e37399256745558f955e5
slobar <slobodan.radosavljevic@vicert.com>
2015-08-11 10:29:50 +0200

    Code clean up.

--- app/controllers/job_scores_controller.rb
+++ app/controllers/job_scores_controller.rb
@@ -1,10 +1,9 @@
 class JobScoresController < ApplicationController
-  before_action :set_job_score, only: [:show, :edit, :update, :destroy]
+  before_action :set_job_score, only: [:show, :destroy]
 
   # GET /job_scores
   # GET /job_scores.json
   def index
-    #@job_scores = JobScore.all
     @job_scores = JobScore.paginate(:page => params[:page], :per_page => 10)
   end
 
@@ -13,44 +12,6 @@ class JobScoresController < ApplicationController
   def show
   end
 
-  # GET /job_scores/new
-  def new
-    @job_score = JobScore.new
-  end
-
-  # GET /job_scores/1/edit
-  def edit
-  end
-
-  # POST /job_scores
-  # POST /job_scores.json
-  def create
-    @job_score = JobScore.new(job_score_params)
-
-    respond_to do |format|
-      if @job_score.save
-        format.html { redirect_to @job_score, notice: 'Job score was successfully created.' }
-        format.json { render :show, status: :created, location: @job_score }
-      else
-        format.html { render :new }
-        format.json { render json: @job_score.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /job_scores/1
-  # PATCH/PUT /job_scores/1.json
-  def update
-    respond_to do |format|
-      if @job_score.update(job_score_params)
-        format.html { redirect_to @job_score, notice: 'Job score was successfully updated.' }
-        format.json { render :show, status: :ok, location: @job_score }
-      else
-        format.html { render :edit }
-        format.json { render json: @job_score.errors, status: :unprocessable_entity }
-      end
-    end
-  end
 
   # DELETE /job_scores/1
   # DELETE /job_scores/1.json
@@ -70,6 +31,6 @@ class JobScoresController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def job_score_params
-      params.require(:job_score).permit(:job, :start_time, :end_time)
+      params.require(:job_score).permit(:job, :start_time, :end_time, :status)
     end
 end
